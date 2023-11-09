f9046c21ad2671115413bffb5cc6415cf960e203
CoadingClass <129182183+CoadingClass@users.noreply.github.com>
2023-11-09 09:53:52 +0000

    DataType

---
+++ _01Basic/01_variables.js
@@ -0,0 +1,8 @@
+const constage = 22;
+let letname = "Govind";
+var varemail = "govind@gmail.com";
+id = 333;
+
+// Prefer not to use var because of issue in block and functional scope
+
+console.table([constage, letname, varemail, id])
---
+++ _01Basic/02_dataType.js
@@ -0,0 +1,13 @@
+"use strict"; //treate all js code as newer version
+
+// alert("Hello") it will not work because we are using nodejs not browser
+
+// number => 2 to power 53
+// bigint 
+// string => "" 
+// boolean => true/false 
+// null => standalone value 
+// undefine => 
+// symbol => unique 
+// object 
+console.log(typeof null)
